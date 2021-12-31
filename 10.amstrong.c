a8cd121826dfcaf5b66d16c0025b47caba942d80
jesvin618 <96870372+jesvin618@users.noreply.github.com>
2021-12-31 14:09:23 +0530

    Add files via upload

---
+++ 10.amstrong.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(void)
+{
+  int n,r,sum=0,var;
+  printf("Enter a number:");
+  scanf("%d",&n);
+  var=n;
+  while(n>0)
+  {
+    r=n%10;
+    n=n/10;
+    sum=sum+(r*r*r);
+  }
+  if(var==sum)
+  {
+    printf("The number is an Armstrong");
+  }
+  else
+  {
+    printf("The number is not an Armstrong");
+  }
+  return 0;
+}
